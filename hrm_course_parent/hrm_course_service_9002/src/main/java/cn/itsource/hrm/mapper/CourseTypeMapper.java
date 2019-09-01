3c722dd5a77b68ba2bb0861acbd70c21101c353e
flandrez <a3705348.163.com>
2019-09-01 10:15:39 +0800

    提交

---
+++ hrm_course_parent/hrm_course_service_9002/src/main/java/cn/itsource/hrm/mapper/CourseTypeMapper.java
@@ -0,0 +1,16 @@
+package cn.itsource.hrm.mapper;
+
+import cn.itsource.hrm.domain.CourseType;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程目录 Mapper 接口
+ * </p>
+ *
+ * @author yhptest
+ * @since 2019-08-31
+ */
+public interface CourseTypeMapper extends BaseMapper<CourseType> {
+
+}
